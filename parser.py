0f73e607ac2a843b40e28a83872a099e6ab5f49b
tsundokul <timofte17daniel@gmail.com>
2024-01-13 22:53:47 +0200

    Add arg parser, split base class

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import glom
 import logging
 import orjson
@@ -12,22 +13,90 @@ from rich import progress as prog
 
 class Parser:
     def __init__(self, user_options: dict = {}) -> None:
-        self.opt = {"verbose": False, "secure": True, "threads": 5}
-        self.opt.update(user_options)
-        self.es_client = Elasticsearch(self.opt["es_host"])
-        self.log = self.__make_logger()
+        self.opt = user_options
+        self.es_client = Elasticsearch(self.opt["es_host"] or os.environ["ES_HOST"])
+        self.log = self._make_logger()
         self.api = pisicap.api.SICAP(
             secure=self.opt["secure"], verbose=self.opt["verbose"]
         )
 
-    def get_notices_list(self) -> dict:
-        options = { "pageSize": 3000 }
-        if self.opt.get('startDate'):
-            options["startPublicationDate"] = self.opt['startDate']
-        if self.opt.get('endDate'):
-            options["endPublicationDate"] = self.opt['endDate']
+    def main(self):
+        raise NotImplementedError()
 
-        response = self.api.getCNoticeList(options)
+    @staticmethod
+    def _string_to_int(text: str) -> str:
+        return int(re.sub(r"\D", "", text, flags=re.IGNORECASE))
+
+    def _clean_fiscal_code(self, code: str) -> int:
+        if not isinstance(code, str):
+            code = "0"
+
+        return self._string_to_int(code)
+
+    def _multithread_run(self, task, args_iter):
+        results = []
+
+        with ThreadPoolExecutor(max_workers=self.opt["threads"]) as executor:
+            with prog.Progress(
+                prog.BarColumn(),
+                prog.TaskProgressColumn(),
+                prog.TimeRemainingColumn(),
+                prog.MofNCompleteColumn(),
+            ) as prog_bar:
+                futures = [executor.submit(task, i) for i in args_iter]
+                task = prog_bar.add_task("👾", total=len(futures))
+
+                for f in as_completed(futures):
+                    try:
+                        results.append(f.result())
+                    except Exception as e:
+                        self.log.error(e, exc_info=True)
+                    finally:
+                        prog_bar.advance(task)
+
+        return results
+
+    def _upsert_es_doc(self, doc, doc_id):
+        return self.es_client.update(
+            index=self.opt["es_index"],
+            id=doc_id,
+            doc=doc,
+            doc_as_upsert=True,
+        )
+
+    def _make_logger(self):
+        lformat = "%(asctime)s - %(levelname)s - %(message)s"
+        formatter = logging.Formatter(lformat)
+        logging.basicConfig(format=lformat)
+
+        logger = logging.getLogger("pisicap-parser")
+        logger.setLevel(logging.DEBUG if self.opt["verbose"] else logging.INFO)
+
+        file_handler = logging.FileHandler("errors.log")
+        file_handler.setLevel(logging.ERROR)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        return logger
+
+
+# CAN stands for Contract Award Notices
+class ParserCAN(Parser):
+    def __init__(self, user_options: dict = {}) -> None:
+        if "es_index" not in user_options:
+            user_options["es_index"] = "licitatii-publice"
+
+        super().__init__(user_options)
+        self.log.info("Contract Award Notices mode")
+
+    def get_notices_list(self) -> dict:
+        options = {"pageSize": 3000}
+        if self.opt.get("date"):
+            options["startPublicationDate"] = self.opt["startDate"]
+        if self.opt.get("end_date"):
+            options["endPublicationDate"] = self.opt["endDate"]
+
+        response = self.api.getCANoticeList(options)
         return orjson.loads(response.text)
 
     def get_notice(self, id_anunt: int) -> dict:
@@ -55,7 +124,7 @@ class Parser:
 
         item["cpvCode"] = item["cpvCodeAndName"].split(" - ")[0]
         authority = item["contractingAuthorityNameAndFN"].split("-")[0]
-        item["nationalId"] = self.__clean_fiscal_code(authority)
+        item["nationalId"] = self._clean_fiscal_code(authority)
 
         return item
 
@@ -147,7 +216,7 @@ class Parser:
             f"caNoticeEdit_New{u}.section1_New{u}.section1_1.caAddress.nationalIDNumber"
         )
         glom.assign(
-            notice, f"{path}Int", self.__clean_fiscal_code(glom.glom(notice, path))
+            notice, f"{path}Int", self._clean_fiscal_code(glom.glom(notice, path))
         )
 
     def __clean_contract(self, contract: dict) -> None:
@@ -174,8 +243,11 @@ class Parser:
             glom.glom(contract, "winner"),
             *(glom.glom(contract, "winners") or []),
         ]:
+            if not g:
+                continue
+
             glom.assign(
-                g, "fiscalNumberInt", self.__clean_fiscal_code(g.get("fiscalNumber"))
+                g, "fiscalNumberInt", self._clean_fiscal_code(g.get("fiscalNumber"))
             )
 
             if ga := glom.glom(g, "address"):
@@ -185,29 +257,17 @@ class Parser:
                 glom.assign(
                     ga,
                     "nationalIDNumberInt",
-                    self.__clean_fiscal_code(ga.get("nationalIDNumber")),
+                    self._clean_fiscal_code(ga.get("nationalIDNumber")),
                 )
 
-    @staticmethod
-    def __string_to_int(text: str) -> str:
-        return re.sub(r"\D", "", text, flags=re.IGNORECASE)
-
-    def __clean_fiscal_code(self, code: str) -> int:
-        if not isinstance(code, str):
-            code = "0"
-
-        return int(self.__string_to_int(code))
-
     def main(self) -> None:
-        results = self.parse_notices()
-        breakpoint
-        return results
+        return self.parse_notices()
 
     def parse_notices(self):
         notices_list = self.get_notices_list()
         items = list(map(self.__clean_item, notices_list["items"]))
         self.log.info(f"Total notices to fetch: {notices_list['total']}")
-        return self.__multithread_run(self.add_notice, items)
+        return self._multithread_run(self.add_notice, items)
 
     def add_notice(self, item: dict):
         notice = self.get_notice(item["caNoticeId"])
@@ -223,60 +283,50 @@ class Parser:
             "noticeContracts": contracts,
             "istoric": False,
         }
-        return self.__upsert_es_doc(es_doc, item["caNoticeId"])
+        return self._upsert_es_doc(es_doc, item["caNoticeId"])
 
-    def __multithread_run(self, task, args_iter):
-        with ThreadPoolExecutor(max_workers=self.opt["threads"]) as executor:
-            with prog.Progress(
-                prog.BarColumn(),
-                prog.TaskProgressColumn(),
-                prog.TimeRemainingColumn(),
-                prog.MofNCompleteColumn(),
-            ) as prog_bar:
-                futures = [executor.submit(task, i) for i in args_iter]
-                task = prog_bar.add_task("👾", total=len(futures))
 
-                results = []
+# Prior information notices
+class ParserPIN(Parser):
+    def __init__(self, user_options: dict = {}) -> None:
+        if "es_index" not in user_options:
+            user_options["es_index"] = "licitatii-deschise"
 
-                for f in as_completed(futures):
-                    try:
-                        results.append(f.result())
-                    except Exception as e:
-                        self.log.error(e, exc_info=True)
-                    finally:
-                        prog_bar.advance(task)
+        super().__init__(user_options)
+        self.log.info("Prior Information Notices mode")
 
-                return results
 
-    def __upsert_es_doc(self, doc, doc_id):
-        return self.es_client.update(
-            index=self.opt["es_index"],
-            id=doc_id,
-            doc=doc,
-            doc_as_upsert=True,
-        )
+def parse_cli_args():
+    parser = argparse.ArgumentParser(
+        prog="SICAP Parser", description="Fetch data from e-licitatie.ro"
+    )
 
-    def __make_logger(self):
-        lformat = "%(asctime)s - %(levelname)s - %(message)s"
-        formatter = logging.Formatter(lformat)
-        logging.basicConfig(format=lformat)
+    parser.add_argument("-v", "--verbose", action="store_true", default=False)
+    parser.add_argument("-s", "--secure", action="store_true", default=True)
+    parser.add_argument("-d", "--date", help="YYYY-MM-DD, defaults to yesterday")
+    parser.add_argument("-e", "--end-date", help="YYYY-MM-DD, defaults to now")
+    parser.add_argument(
+        "-u", "--es-host", help="Elasticsearch URL, can be also set via ES_HOST env var"
+    )
+    parser.add_argument("-t", "--threads", type=int, default=5)
+    parser.add_argument(
+        "-m",
+        "--mode",
+        required=True,
+        choices=["CAN", "PIN"],
+        help="CAN (Contract Award Notices), PIN(Prior Information Notices)",
+    )
 
-        logger = logging.getLogger("pisicap-parser")
-        logger.setLevel(logging.DEBUG if self.opt["verbose"] else logging.INFO)
-
-        file_handler = logging.FileHandler("errors.log")
-        file_handler.setLevel(logging.ERROR)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-
-        return logger
+    args = parser.parse_args()
+    return vars(args)
 
 
 if __name__ == "__main__":
-    options = {
-        "es_host": os.environ["ES_HOST"],
-        "es_index": "licitatii-publice",
-        "startDate": "2024-01-09T00:00:00.000Z"
-    }
-    parser = Parser(options)
+    """TODO:
+    - move the classes to separate files
+    - add retry mechanism for failed requests
+    """
+    args = parse_cli_args()
+    parser_class = globals()[f"Parser{args['mode']}"]
+    parser = parser_class(args)
     parser.main()
